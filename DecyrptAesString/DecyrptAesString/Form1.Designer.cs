ef889baaf15a92d2a3be9218ab5557f3b41cfd84
Amit Virmani <virmania@vmware.com>
2018-07-11 10:54:55 +0530

    decyrypt AES string using a hardcoded key

---
+++ DecyrptAesString/DecyrptAesString/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace DecyrptAesString
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtDecyrpted = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(59, 32);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(139, 22);
+            this.txtPassword.TabIndex = 0;
+            // 
+            // txtDecyrpted
+            // 
+            this.txtDecyrpted.Location = new System.Drawing.Point(59, 113);
+            this.txtDecyrpted.Name = "txtDecyrpted";
+            this.txtDecyrpted.Size = new System.Drawing.Size(139, 22);
+            this.txtDecyrpted.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Decyrpt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDecyrpted);
+            this.Controls.Add(this.txtPassword);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtDecyrpted;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DecyrptAesString/DecyrptAesString/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DecyrptAesString
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public static string DecryptString(string cipherText, string key)
+        {
+            string saltValue = "111111111";
+            string hashAlgorithm = "SHA2";
+            int passwordIterations = 2;
+            string initVector = "@111111111";
+            int keySize = 256;
+            return Decrypt(cipherText,
+                key,
+                saltValue,
+                hashAlgorithm,
+                passwordIterations,
+                initVector,
+                keySize);
+        }
+
+
+        public static string Decrypt(string cipherText,
+                                     string passPhrase,
+                                     string saltValue,
+                                     string hashAlgorithm,
+                                     int passwordIterations,
+                                     string initVector,
+                                     int keySize)
+        {
+            if (string.IsNullOrEmpty(cipherText))//nothing to do
+                return cipherText;
+            // Convert strings defining encryption key characteristics into byte
+            // arrays. Let us assume that strings only contain ASCII codes.
+            // If strings include Unicode characters, use Unicode, UTF7, or UTF8
+            // encoding.
+            byte[] initVectorBytes = Encoding.ASCII.GetBytes(initVector);
+            byte[] saltValueBytes = Encoding.ASCII.GetBytes(saltValue);
+
+            // Convert our ciphertext into a byte array.
+
+            byte[] cipherTextBytes = null;
+            try
+            {
+                cipherTextBytes = Convert.FromBase64String(cipherText);
+            }
+            catch (Exception)
+            {
+                return cipherText;
+            }
+
+
+            // First, we must create a password, from which the key will be 
+            // derived. This password will be generated from the specified 
+            // passphrase and salt value. The password will be created using
+            // the specified hash algorithm. Password creation can be done in
+            // several iterations.
+            PasswordDeriveBytes password = new PasswordDeriveBytes(
+                passPhrase,
+                saltValueBytes,
+                hashAlgorithm,
+                passwordIterations);
+
+            // Use the password to generate pseudo-random bytes for the encryption
+            // key. Specify the size of the key in bytes (instead of bits).
+            byte[] keyBytes = password.GetBytes(keySize / 8);
+
+            // Create uninitialized Rijndael encryption object.
+            //RijndaelManaged symmetricKey = new RijndaelManaged();
+            //FIPS verified AES encryption
+            AesCryptoServiceProvider symmetricKey = new AesCryptoServiceProvider();
+
+            // It is reasonable to set encryption mode to Cipher Block Chaining
+            // (CBC). Use default options for other symmetric key parameters.
+            symmetricKey.Mode = CipherMode.CBC;
+
+            // Generate decryptor from the existing key bytes and initialization 
+            // vector. Key size will be defined based on the number of the key 
+            // bytes.
+            ICryptoTransform decryptor = symmetricKey.CreateDecryptor(
+                keyBytes,
+                initVectorBytes);
+
+            // Define memory stream which will be used to hold encrypted data.
+            MemoryStream memoryStream = new MemoryStream(cipherTextBytes);
+
+            // Define cryptographic stream (always use Read mode for encryption).
+            CryptoStream cryptoStream = new CryptoStream(memoryStream,
+                                                         decryptor,
+                                                         CryptoStreamMode.Read);
+
+            // Since at this point we don't know what the size of decrypted data
+            // will be, allocate the buffer long enough to hold ciphertext;
+            // plaintext is never longer than ciphertext.
+            byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+
+            // Start decrypting.
+            int decryptedByteCount = cryptoStream.Read(plainTextBytes,
+                                                       0,
+                                                       plainTextBytes.Length);
+
+            // Close both streams.
+            memoryStream.Close();
+            cryptoStream.Close();
+
+            // Convert decrypted data into a string. 
+            // Let us assume that the original plaintext string was UTF8-encoded.
+            string plainText = Encoding.UTF8.GetString(plainTextBytes,
+                                                       0,
+                                                       decryptedByteCount);
+
+            // Return decrypted string.   
+            return plainText;
+        }
+    }
+}
